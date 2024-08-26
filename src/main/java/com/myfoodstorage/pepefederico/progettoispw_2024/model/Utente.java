fe73a4db5d85b585c5c1063a3056ff63fda5aded
Pepe Federico <pepe.00.fp@outlook.it>
2024-08-26 13:00:35 +0200

    Correzioni

--- src/main/java/com/myfoodstorage/pepefederico/progettoispw_2024/model/Utente.java
+++ src/main/java/com/myfoodstorage/pepefederico/progettoispw_2024/model/Utente.java
@@ -37,99 +37,53 @@ public abstract class Utente {
         this.nomeAttivita = nomeAttivita;
         this.numeroTelefono = numeroTelefono;
     }
+
     public String getTipoUtente() {
         return tipoUtente;
     }
-
     public void setTipoUtente(String tipoUtente) {
         this.tipoUtente = tipoUtente;
     }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getCognome() {
         return cognome;
     }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
     public String getPartitaIva() {
         return partitaIva;
     }
-
-    public void setPartitaIva(String partitaIva) {
-        this.partitaIva = partitaIva;
-    }
-
     public String getViaAttivita() {
         return viaAttivita;
     }
-
-    public void setViaAttivita(String viaAttivita) {
-        this.viaAttivita = viaAttivita;
-    }
-
     public int getNumeroCivico() {
         return numeroCivico;
     }
-
-    public void setNumeroCivico(int numeroCivico) {
-        this.numeroCivico = numeroCivico;
-    }
-
     public String getCitta() {
         return citta;
     }
-
-    public void setCitta(String citta) {
-        this.citta = citta;
-    }
-
     public String getCap() {
         return cap;
     }
-
-    public void setCap(String cap) {
-        this.cap = cap;
-    }
-
     public String getNomeAttivita() {
         return nomeAttivita;
     }
-
     public void setNomeAttivita(String nomeAttivita) {
         this.nomeAttivita = nomeAttivita;
     }
-
     public String getNumeroTelefono() {
         return numeroTelefono;
-    }
-
-    public void setNumeroTelefono(String numeroTelefono) {
-        this.numeroTelefono = numeroTelefono;
     }
 }
