34732a83b3192a3b570c4c90f51de5849553f3e6
Pepe Federico <pepe.00.fp@outlook.it>
2024-08-28 15:33:17 +0200

    Correzioni

--- src/main/java/com/myfoodstorage/pepefederico/progettoispw_2024/App.java
+++ src/main/java/com/myfoodstorage/pepefederico/progettoispw_2024/App.java
@@ -1,10 +1,30 @@
 package com.myfoodstorage.pepefederico.progettoispw_2024;
+
 import com.myfoodstorage.pepefederico.progettoispw_2024.model.Model;
 import javafx.stage.Stage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.RejectedExecutionException;
 
 public class App extends javafx.application.Application {
     @Override
     public void start(Stage stage){
-        Model.getInstance().getViewFactory().showLoginWindow();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Benvenuto in MyFoodStorage, la prima dispensa digitale !");
+
+            System.out.println("Quale GUI vuoi caricare ? 1 --> JavaFx, 2 --> CLI");
+            int guiSelector = Integer.parseInt(reader.readLine());
+
+            if (guiSelector == 1) {
+                Model.getInstance().getViewFactory().showLoginWindow();
+            } else if (guiSelector == 2) {
+                Model.getInstance().getViewFactoryCLI().loginUser();
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            throw new RejectedExecutionException(e);
+        }
     }
 }
